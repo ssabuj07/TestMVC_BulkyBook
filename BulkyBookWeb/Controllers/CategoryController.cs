3ce1289c037ceab80cf701c5dc761f22b15fea98
ssabuj07 <ssabuj07@gmail.com>
2023-06-01 20:05:42 +0600

    first commit

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //List<Category> objCategoryList = _db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");  
+            }
+            if (ModelState.IsValid)  // server side validation is working
+                // determind if the model is valid or not, that means Name is required here, name should have the value
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+            //return RedirectToAction("Index","controllerName"); // if you are redirecting to the other controller. 
+        }
+    }
+}
